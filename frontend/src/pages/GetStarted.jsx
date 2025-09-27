deab04afc5efe58909b32b51f0ad4ccc51449af5
Matthew Zimmerman <mjzimme3@asu.edu>
2025-09-27 15:32:30 -0700

    added react-router + Getstarted + Homepage

---
+++ frontend/src/pages/GetStarted.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const GetStarted = () => {
+  return (
+    <>
+    <h1>Welcome to The Phish Scanner app</h1>
+    <div>This app will help you identify possible phishing emails</div>
+    <div>Click on the "Get Started" button to get started</div>
+    </>
+  )
+}
+
+export default GetStarted
